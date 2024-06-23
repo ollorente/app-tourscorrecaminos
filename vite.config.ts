23d1abd6a0ea2488f387702a2ad243054a21f4cf
Óscar Llorente <llorente_oscar@hotmail.com>
2024-06-23 17:03:23 -0500

    Ajustando

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "https://ollorente.github.io/app-tourscorrecaminos /"
+  base: "https://ollorente.github.io/app-tourscorrecaminos/"
 })
